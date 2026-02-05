e2b20eac2f2616d623e98b59471397392705615b
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2026-02-05 16:49:24 +0100

    feat: rename Terraform configuration files for improved organization

--- src/infra/src/02_core/20_idvh.tf
+++
@@ -1,6 +0,0 @@
-module "idvh_sample" {
-  source = "./modules/idvh_sample"
-
-  key_name = "${local.project}-idvh"
-  tags     = module.tag_config.tags
-}
--- src/infra/src/02_core/modules/idvh_sample/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "key_pair_name" {
-  value       = module.key_pair.key_pair_name
-  description = "Name of the created key pair."
-}
-
-output "key_pair_fingerprint" {
-  value       = module.key_pair.key_pair_fingerprint
-  description = "Fingerprint of the created key pair."
-}
