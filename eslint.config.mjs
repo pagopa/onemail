76f6ce46802d0b230f5afc835a278e74d30fe544
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2026-02-13 18:23:30 +0100

    feat: add trivago prettier plugin for sorting import on save

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,12 @@ export default [
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
       '@typescript-eslint/consistent-generic-constructors': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'perfectionist/sort-objects': 'off',
+      'perfectionist/sort-union-types': 'off',
     },
   },
 ];
