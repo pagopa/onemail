1f62a1d9f26c3031d726e0f314b01ac7dec39596
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2026-02-13 18:33:46 +0100

    chore: prettier

--- src/onemail/om-lambda-sender/src/app.ts
+++ src/onemail/om-lambda-sender/src/app.ts
@@ -1,9 +1,9 @@
-import type { Context } from 'aws-lambda';
-
 import { Router } from '@aws-lambda-powertools/event-handler/http';
+import { Context } from 'aws-lambda';
 
 const app: Router = new Router();
 
 app.get('/', async () => ({ message: 'Hello World!' }));
 
-export const handler = async (event: unknown, context: Context) => app.resolve(event, context);
+export const handler = async (event: unknown, context: Context) =>
+  app.resolve(event, context);
