890ce47d02b13dbaa9d7b2e5cb509f9467041f31
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-02-25 18:38:07 +0100

    feat: Create rest api with vpc endpoint config

---
+++ src/infra/src/70_domains/onemail_app/01_api_gtw.tf
@@ -0,0 +1,42 @@
+data "aws_vpc_endpoint" "api_gtw" {
+  service_name = "com.amazonaws.eu-south-1.execute-api"
+}
+
+resource "aws_api_gateway_rest_api_policy" "api_policy" {
+  rest_api_id = module.api_gateway.rest_api_id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "execute-api:Invoke",
+        Resource  = "${module.api_gateway.rest_api_execution_arn}/*"
+      },
+      {
+        Effect    = "Deny",
+        Principal = "*",
+        Action    = "execute-api:Invoke",
+        Resource  = "${module.api_gateway.rest_api_execution_arn}/*",
+        Condition = {
+          StringNotEquals = {
+            "aws:sourceVpce" : data.aws_vpc_endpoint.api_gtw.id
+          }
+        }
+      }
+    ]
+  })
+}
+
+module "api_gateway" {
+  source = "./.terraform/modules/aws_modules/IDVH/api_gateway"
+
+  env                       = var.env
+  product_name              = "onemail"
+  idvh_resource_tier        = "standard"
+  name                      = "${local.project}-api-gateway"
+  body                      = ""
+  endpoint_api_types        = ["PRIVATE"]
+  endpoint_vpc_endpoint_ids = [data.aws_vpc_endpoint.api_gtw.id]
+}
