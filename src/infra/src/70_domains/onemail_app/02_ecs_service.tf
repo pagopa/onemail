17b1584378f6b80f19a3624328426bf5d337b6c0
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-03-02 17:15:04 +0100

    fix: add container_port to ECS service module configuration

--- src/infra/src/70_domains/onemail_app/02_ecs_service.tf
+++ src/infra/src/70_domains/onemail_app/02_ecs_service.tf
@@ -47,6 +47,7 @@ module "ecs_service" {
   cluster_arn           = data.aws_ecs_cluster.core.arn
   image                 = var.use_placeholder_image ? "nginx:latest" : "${data.aws_ecr_repository.ecs_service.repository_url}:${var.ecs_service_image_version}"
   container_name        = "${local.project_nodomain}-ecs-container"
+  container_port        = 8080
   private_subnets       = data.aws_subnets.private.ids
   target_group_arn      = data.aws_lb_listener.ecs_core.default_action[0].target_group_arn
   nlb_security_group_id = element(tolist(data.aws_lb.nlb.security_groups), 0)
