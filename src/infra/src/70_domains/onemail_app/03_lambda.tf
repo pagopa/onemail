d8767a013c687a0c89a0158f8198a3cf340d5608
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-03-06 16:44:02 +0100

    feat: Implement Lambda function for email processing with SQS integration

---
+++ src/infra/src/70_domains/onemail_app/03_lambda.tf
@@ -0,0 +1,78 @@
+data "aws_sqs_queue" "high_priority" {
+  name = "${local.project_nodomain}-sqs-high-priority"
+}
+
+data "aws_sqs_queue" "low_priority" {
+  name = "${local.project_nodomain}-sqs-low-priority"
+}
+
+data "aws_iam_policy_document" "sender_policy" {
+  statement {
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = [
+      data.aws_sqs_queue.high_priority.arn,
+      data.aws_sqs_queue.low_priority.arn
+    ]
+  }
+
+  statement {
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "security_group_lambda_sender" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+
+  name        = "${local.project_nodomain}-sg-lambda-sender"
+  description = "Security group for sender lambda"
+  vpc_id      = data.aws_vpc.core.id
+
+  egress_cidr_blocks      = []
+  egress_ipv6_cidr_blocks = []
+
+  egress_rules = ["https-443-tcp"]
+}
+
+module "lambda_sender" {
+  source = "./.terraform/modules/aws_modules/IDVH/lambda"
+
+  env                = var.env
+  product_name       = "onemail"
+  idvh_resource_tier = "standard"
+  github_repository  = var.github_repository
+
+  name        = "${local.project_nodomain}-lambda-sender"
+  description = "Lambda function responsible for sending emails, triggered by SQS messages from both high and low priority queues."
+
+  package_path       = "${path.module}/${var.lambda_sender.package_path}"
+  lambda_policy_json = data.aws_iam_policy_document.sender_policy.json
+
+  memory_size                    = 256
+  reserved_concurrent_executions = var.lambda_sender.reserved_concurrent_executions
+  environment_variables          = var.lambda_sender.environment_variables
+  vpc_subnet_ids                 = data.aws_subnets.private.ids
+  vpc_security_group_ids         = [module.security_group_lambda_sender.security_group_id]
+
+  tags = module.tag_config.tags
+}
+
+resource "aws_lambda_event_source_mapping" "high_priority_sender" {
+  event_source_arn = data.aws_sqs_queue.high_priority.arn
+  function_name    = module.lambda_sender.lambda_function_arn
+  scaling_config { maximum_concurrency = 8 } # To adjust based on expected load for high priority tasks
+}
+
+resource "aws_lambda_event_source_mapping" "low_priority_sender" {
+  event_source_arn = data.aws_sqs_queue.low_priority.arn
+  function_name    = module.lambda_sender.lambda_function_arn
+  scaling_config { maximum_concurrency = 2 } # To adjust based on expected load for low priority tasks
+}
--- src/infra/src/70_domains/onemail_app/env/prod/eu-south-1/terraform.tfvars
+++ src/infra/src/70_domains/onemail_app/env/prod/eu-south-1/terraform.tfvars
@@ -1,12 +1,12 @@
 # general
-prefix         = "oml"
-env_short      = "p"
-env            = "prod"
-domain         = "onemail_app"
-location       = "eu-south"
-location_short = "eus1"
-aws_region     = "eu-south-1"
-
+prefix            = "oml"
+env_short         = "p"
+env               = "prod"
+domain            = "onemail_app"
+location          = "eu-south"
+location_short    = "eus1"
+aws_region        = "eu-south-1"
+github_repository = "pagopa/onemail"
 
 # API Gateway
 api_gateway_deployment_version = "1.0.0"
@@ -14,3 +14,10 @@ api_gateway_deployment_version = "1.0.0"
 # ECS Service
 ecs_service_image_name    = "core"
 ecs_service_image_version = "latest" #Temporary version to test ECS Service
+
+# Lambda Sender
+lambda_sender = {
+  package_path                   = "lambda/hello-nodejs/hello-nodejs.zip"
+  reserved_concurrent_executions = 10
+  environment_variables          = {}
+}
