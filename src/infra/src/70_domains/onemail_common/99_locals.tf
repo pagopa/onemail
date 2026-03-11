5c6c3178e6232787a1c0d2effaa1e8e10aa6b6ac
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-03-11 10:11:03 +0100

    fix: Update AWS provider version and refactor S3 bucket module configuration (#56)

--- src/infra/src/70_domains/onemail_common/99_locals.tf
+++ src/infra/src/70_domains/onemail_common/99_locals.tf
@@ -1,6 +1,5 @@
 locals {
-  project            = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
-  project_nodomain   = "${var.prefix}-${var.env_short}-${var.location_short}"
-  product            = "${var.prefix}-${var.env_short}"
-  bucket_lambda_code = "lambda-code-${random_integer.bucket_lambda_code_suffix.result}"
+  project          = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  project_nodomain = "${var.prefix}-${var.env_short}-${var.location_short}"
+  product          = "${var.prefix}-${var.env_short}"
 }
