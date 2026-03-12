8f497b5d48606a93522cfbb6a42a5241828be1f5
BenSca <120383035+BenSca@users.noreply.github.com>
2026-03-12 11:04:24 +0000

    chore: add RequestIdSchema

--- src/onemail/om-ecs-dispatcher/src/dtos/email/common.dto.ts
+++ src/onemail/om-ecs-dispatcher/src/dtos/email/common.dto.ts
@@ -79,12 +79,12 @@ export const DryRunQueryParamsSchema = z
     },
   );
 
+export const RequestIdSchema = stringCheckedSchema().describe(
+  'Unique identifier for the request, used for checking the status of the email in the system',
+);
+
 export const EmailSuccessResponseSchema = z.object({
-  requestId: z
-    .string()
-    .describe(
-      'Unique identifier for the request, used for checking the status of the email in the system',
-    ),
+  requestId: RequestIdSchema,
 });
 
 export const htmlContentSchema = stringCheckedSchema({ min: 10, max: 200000 })
