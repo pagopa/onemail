d8eba6e131755cbd09b19636e6fa88b6cd23759d
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2026-03-12 18:38:47 +0100

    chore: remove todo for env var region

--- src/onemail/om-lambda-sender/src/connector/ses.connector.ts
+++ src/onemail/om-lambda-sender/src/connector/ses.connector.ts
@@ -2,4 +2,3 @@ import { SESv2Client } from '@aws-sdk/client-sesv2'; // ES Modules import
 
 const config = {}; // type is SESv2ClientConfig
 export const sesClient = new SESv2Client(config);
-//TODO - check if region should be set
