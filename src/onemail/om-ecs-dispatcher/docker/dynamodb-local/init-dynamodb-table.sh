16cb8d52167bd1cfe20712af31ea4df7a13383b3
BenSca <120383035+BenSca@users.noreply.github.com>
2026-03-12 12:38:21 +0100

    chore: add gsi init table and env var

--- src/onemail/om-ecs-dispatcher/docker/dynamodb-local/init-dynamodb-table.sh
+++ src/onemail/om-ecs-dispatcher/docker/dynamodb-local/init-dynamodb-table.sh
@@ -26,19 +26,33 @@ echo "Region: $REGION"
 AWS_ACCESS_KEY_ID_VALUE="${AWS_DYNAMODB_ACCESS_KEY_ID:-local}"
 AWS_SECRET_ACCESS_KEY_VALUE="${AWS_DYNAMODB_SECRET_ACCESS_KEY:-local}"
 
+# TODO use same index-nama as terraform variable
 AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_VALUE" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_VALUE" \
   aws dynamodb create-table \
   --table-name "$TABLE_NAME" \
   --attribute-definitions \
     AttributeName=emailId,AttributeType=S \
+    AttributeName=requestId,AttributeType=S \
   --key-schema \
     AttributeName=emailId,KeyType=HASH \
+   --global-secondary-indexes \
+    "[
+      {
+        \"IndexName\": \"requestId-index\",
+        \"KeySchema\": [
+          {\"AttributeName\": \"requestId\", \"KeyType\": \"HASH\"}
+        ],
+        \"Projection\": {
+          \"ProjectionType\": \"ALL\"
+        }
+      }
+    ]" \
   --billing-mode PAY_PER_REQUEST \
   --endpoint-url "$ENDPOINT" \
   --region "$REGION" \
   --no-cli-pager
 
 echo "Table $TABLE_NAME created successfully"
-# To verify, run: 
+# To verify, run:
 # AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_VALUE" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_VALUE" \
 # aws dynamodb describe-table --table-name $TABLE_NAME --endpoint-url $ENDPOINT --region $REGION"
