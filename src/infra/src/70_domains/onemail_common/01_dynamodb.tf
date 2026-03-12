b3370d128c6a5bb1ffae83c166f356294770d465
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-03-12 16:18:35 +0100

    feat: Update DynamoDB configuration and add global secondary index support (#65)
    * feat: Update DynamoDB configuration and add global secondary index support
    
    * feat: Remove empty global secondary indexes from DynamoDB configuration
    
    * chore: terraform format

--- src/infra/src/70_domains/onemail_common/01_dynamodb.tf
+++ src/infra/src/70_domains/onemail_common/01_dynamodb.tf
@@ -1,15 +1,14 @@
 module "dynamodb_table" {
   count  = var.dynamodb_table_config != null ? 1 : 0
-  source = "./.terraform/modules/aws_modules/IDVH/dynamodb"
+  source = "git::https://github.com/pagopa/technology-aws-modules.git//IDVH/dynamodb?ref=main"
 
   env                = var.env
   product_name       = "onemail"
   idvh_resource_tier = "standard"
 
-  table_config       = var.dynamodb_table_config # pass-through diretto
-  create_kms_key     = var.dynamodb_table_config.create_kms_key
-  kms_alias          = var.dynamodb_table_config.kms_alias
-  enable_replication = var.dynamodb_enable_replication
+  table_config   = var.dynamodb_table_config # pass-through diretto
+  create_kms_key = var.dynamodb_table_config.create_kms_key
+  kms_alias      = var.dynamodb_table_config.kms_alias
   tags = merge(
     module.tag_config.tags,
     {
