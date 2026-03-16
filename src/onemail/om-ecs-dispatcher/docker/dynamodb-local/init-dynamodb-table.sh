06ea21cba7d7b94ff37d85a18d6e70dc4072f996
BenSca <120383035+BenSca@users.noreply.github.com>
2026-03-16 11:50:35 +0100

    chore: add GSI_NAME variable in init dynamo sh

--- src/onemail/om-ecs-dispatcher/docker/dynamodb-local/init-dynamodb-table.sh
+++ src/onemail/om-ecs-dispatcher/docker/dynamodb-local/init-dynamodb-table.sh
@@ -17,6 +17,9 @@ TABLE_NAME="${AWS_EMAIL_DB_TABLE:-EmailStatusHistory}"
 ENDPOINT="${AWS_DYNAMODB_ENDPOINT:-http://localhost:8000}"
 REGION="${AWS_REGION:-eu-south-1}"
 
+# Global secondary index name (can be overridden from environment)
+GSI_NAME="${AWS_EMAIL_DB_REQUEST_ID_GSI:-gsi_request_id_idx}"
+
 echo "Creating DynamoDB table: $TABLE_NAME"
 echo "Endpoint: $ENDPOINT"
 echo "Region: $REGION"
@@ -26,7 +29,7 @@ echo "Region: $REGION"
 AWS_ACCESS_KEY_ID_VALUE="${AWS_DYNAMODB_ACCESS_KEY_ID:-local}"
 AWS_SECRET_ACCESS_KEY_VALUE="${AWS_DYNAMODB_SECRET_ACCESS_KEY:-local}"
 
-# TODO use same index-nama as terraform variable
+# TODO: align GSI name with Terraform variable; use AWS_EMAIL_DB_REQUEST_ID_GSI
 AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_VALUE" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_VALUE" \
   aws dynamodb create-table \
   --table-name "$TABLE_NAME" \
@@ -38,7 +41,7 @@ AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_VALUE" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_
    --global-secondary-indexes \
     "[
       {
-        \"IndexName\": \"gsi_request_id_idx\",
+        \"IndexName\": \"${GSI_NAME}\",
         \"KeySchema\": [
           {\"AttributeName\": \"requestId\", \"KeyType\": \"HASH\"}
         ],
