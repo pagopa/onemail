02cd59fe2ac5438968e67eee6a19ad0c3768c450
BenSca <120383035+BenSca@users.noreply.github.com>
2026-03-18 13:45:38 +0100

    chore: set terraform version using feature and set node version using nvm

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,16 @@ set -euo pipefail
 
 cd /workspaces/onemail
 
+echo "==> Setup Node from .nvmrc"
+
+source /opt/nvm/nvm.sh 2>/dev/null || true
+nvm install
+nvm use
+
+echo "==> Enable pnpm"
 corepack enable
-pnpm config set store-dir /home/vscode/.pnpm-store || pnpm config set store-dir /home/node/.pnpm-store
+
+pnpm config set store-dir /home/vscode/.pnpm-store
+
 pnpm install
 pnpm setup:hooks
