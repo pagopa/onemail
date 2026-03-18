750358195ad5f57b3f7124dc804011628afacc43
BenSca <120383035+BenSca@users.noreply.github.com>
2026-03-18 11:53:05 +0000

    refactor: use dx container template

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,15 +3,7 @@ set -euo pipefail
 
 cd /workspaces/onemail
 
-source /usr/local/share/nvm/nvm.sh 2>/dev/null || true
-nvm install
-nvm use
-
 corepack enable
-corepack install
-
-# store pnpm packages in the home directory to avoid permission issues
-pnpm config set store-dir /home/node/.pnpm-store
-
+pnpm config set store-dir /home/vscode/.pnpm-store || pnpm config set store-dir /home/node/.pnpm-store
 pnpm install
 pnpm setup:hooks
