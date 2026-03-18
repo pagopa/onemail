51b089370de5e129633aebc1ef06479ddebe604f
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2026-03-18 14:51:59 +0100

    refactor(health service): replace ListTablesCommand with DescribeTableCommand

--- src/onemail/om-ecs-dispatcher/src/services/health.service.ts
+++ src/onemail/om-ecs-dispatcher/src/services/health.service.ts
@@ -1,17 +1,20 @@
+import env from '#config/env';
 import { dynamoClient } from '#connector/dynamo.connector';
 import {
   HealthResponseDTO,
   HealthStatus,
   ServiceStatus,
 } from '#dtos/health/health.dto';
-import { ListTablesCommand } from '@aws-sdk/client-dynamodb';
+import { DescribeTableCommand } from '@aws-sdk/client-dynamodb';
 
 export const healthCheck = async (): Promise<HealthResponseDTO> => {
   let dbStatus = ServiceStatus.Unavailable;
 
   try {
     // lightweight call to verify connectivity
-    await dynamoClient.send(new ListTablesCommand({ Limit: 1 }));
+    await dynamoClient.send(
+      new DescribeTableCommand({ TableName: env.aws.emailDbTable }),
+    );
     dbStatus = ServiceStatus.Initialized;
   } catch {
     dbStatus = ServiceStatus.NotInitialized;
