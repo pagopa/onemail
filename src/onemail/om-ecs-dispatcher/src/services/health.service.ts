b4852ecdbf87bf75d0cf5a5df1e2277bf2cd2b9c
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2026-03-18 15:08:42 +0100

    feat(health service): add SQS queue status checks to health response

--- src/onemail/om-ecs-dispatcher/src/services/health.service.ts
+++ src/onemail/om-ecs-dispatcher/src/services/health.service.ts
@@ -1,17 +1,24 @@
 import env from '#config/env';
 import { dynamoClient } from '#connector/dynamo.connector';
+import { sqsClient } from '#connector/sqs.connector';
 import {
   HealthResponseDTO,
   HealthStatus,
   ServiceStatus,
 } from '#dtos/health/health.dto';
 import { DescribeTableCommand } from '@aws-sdk/client-dynamodb';
+import {
+  GetQueueAttributesCommand,
+  QueueAttributeName,
+} from '@aws-sdk/client-sqs';
 
 export const healthCheck = async (): Promise<HealthResponseDTO> => {
   let dbStatus = ServiceStatus.Unavailable;
+  let sqsHighStatus = ServiceStatus.Unavailable;
+  let sqsLowStatus = ServiceStatus.Unavailable;
 
+  // Lightweight call to verify dynamoDB connectivity
   try {
-    // lightweight call to verify connectivity
     await dynamoClient.send(
       new DescribeTableCommand({ TableName: env.aws.emailDbTable }),
     );
@@ -20,8 +27,36 @@ export const healthCheck = async (): Promise<HealthResponseDTO> => {
     dbStatus = ServiceStatus.NotInitialized;
   }
 
+  // Lightweight call to verify high priority SQS queue connectivity
+  try {
+    await sqsClient.send(
+      new GetQueueAttributesCommand({
+        QueueUrl: env.aws.sqs.highPriorityQueueUrl,
+        AttributeNames: [QueueAttributeName.QueueArn],
+      }),
+    );
+    sqsHighStatus = ServiceStatus.Initialized;
+  } catch {
+    sqsHighStatus = ServiceStatus.NotInitialized;
+  }
+
+  // Lightweight call to verify low priority SQS queue connectivity
+  try {
+    await sqsClient.send(
+      new GetQueueAttributesCommand({
+        QueueUrl: env.aws.sqs.lowPriorityQueueUrl,
+        AttributeNames: [QueueAttributeName.QueueArn],
+      }),
+    );
+    sqsLowStatus = ServiceStatus.Initialized;
+  } catch {
+    sqsLowStatus = ServiceStatus.NotInitialized;
+  }
+
   const overall =
-    dbStatus === ServiceStatus.Initialized
+    dbStatus === ServiceStatus.Initialized &&
+    sqsHighStatus === ServiceStatus.Initialized &&
+    sqsLowStatus === ServiceStatus.Initialized
       ? HealthStatus.Healthy
       : HealthStatus.Unhealthy;
 
@@ -30,6 +65,10 @@ export const healthCheck = async (): Promise<HealthResponseDTO> => {
     timestamp: new Date().toISOString(),
     services: {
       db: dbStatus,
+      queue: {
+        highPriority: sqsHighStatus,
+        lowPriority: sqsLowStatus,
+      },
     },
     uptime: process.uptime(),
   };
