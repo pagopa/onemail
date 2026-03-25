da77ac75d8c426c008e57d29cffcb03315b960b4
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-03-25 17:20:27 +0100

    feat: add Lambda Config Set Processor with IAM policy and security group configuration

--- src/infra/src/70_domains/onemail_app/03_lambda.tf
+++ src/infra/src/70_domains/onemail_app/03_lambda.tf
@@ -1,3 +1,4 @@
+# Lambda Sender
 data "aws_iam_policy_document" "sender_policy" {
   statement {
     actions = [
@@ -127,4 +128,91 @@ resource "aws_lambda_event_source_mapping" "low_priority_sender" {
   event_source_arn = data.aws_sqs_queue.low_priority.arn
   function_name    = module.lambda_sender.lambda_function_arn
   scaling_config { maximum_concurrency = 2 } # To adjust based on expected load for low priority tasks
+}
+
+# Lambda Config Set Processor
+data "aws_iam_policy_document" "set_processor_policy" {
+  statement {
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = ["*"] # Broad permissions for SQS queues, to be refined with specific ARNs when queue is created
+  }
+
+  statement {
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:BatchGetItem"
+    ]
+    resources = [
+      data.aws_dynamodb_table.EmailStatusHistory.arn,
+      "${data.aws_dynamodb_table.EmailStatusHistory.arn}/index/${local.gsi_name}"
+    ]
+  }
+
+  dynamic "statement" {
+    for_each = local.dynamodb_kms_key_arn != null ? [local.dynamodb_kms_key_arn] : []
+
+    content {
+      sid = "KMSAccess"
+
+      actions = [
+        "kms:Decrypt",
+        "kms:Encrypt"
+      ]
+
+      resources = [statement.value]
+    }
+  }
+}
+
+
+module "security_group_lambda_set_processor" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+
+  name        = "${local.project_nodomain}-sg-lambda-config-set-processor"
+  description = "Security group for config set processor lambda"
+  vpc_id      = data.aws_vpc.core.id
+
+  egress_cidr_blocks      = []
+  egress_ipv6_cidr_blocks = []
+
+  egress_prefix_list_ids = [
+    data.aws_vpc_endpoint.dynamodb.prefix_list_id
+  ]
+}
+
+module "lambda_set_processor" {
+  source = "./.terraform/modules/aws_modules/IDVH/lambda"
+
+  env                = var.env
+  product_name       = "onemail"
+  idvh_resource_tier = "standard"
+
+  name        = "${local.project_nodomain}-lambda-config-set-processor"
+  description = "Lambda function responsible for processing config sets, triggered by SQS messages from both high and low priority queues"
+
+  package_path       = "${path.module}/${var.lambda_set_processor.package_path}"
+  lambda_policy_json = data.aws_iam_policy_document.set_processor_policy.json
+
+  memory_size                    = 256
+  reserved_concurrent_executions = var.lambda_set_processor.reserved_concurrent_executions
+  # environment_variables = {
+  #   AWS_EMAIL_DB_TABLE          = data.aws_dynamodb_table.EmailStatusHistory.name
+  #   AWS_EMAIL_DB_REQUEST_ID_GSI = one(data.aws_dynamodb_table.EmailStatusHistory.global_secondary_index).name
+  #   HIGH_PRIORITY_QUEUE_ARN     = data.aws_sqs_queue.high_priority.arn
+  #   LOW_PRIORITY_QUEUE_ARN      = data.aws_sqs_queue.low_priority.arn
+  #   NODE_ENV                    = "production"
+  # }
+  vpc_subnet_ids         = data.aws_subnets.private.ids
+  vpc_security_group_ids = [module.security_group_lambda_set_processor.security_group_id]
+
+  tags = module.tag_config.tags
 }
