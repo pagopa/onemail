9f12b4a194e6a335d0b20a8f548bd86399a4f18b
GiuMontesano <giuseppe.montesano@pagopa.it>
2026-04-10 14:57:19 +0200

    chore: remove todo

--- src/onemail/om-common/src/config/env.ts
+++ src/onemail/om-common/src/config/env.ts
@@ -2,9 +2,8 @@ export default {
   projectVersion: process.env.npm_package_version || '1.0.0',
   aws: {
     cloudWatchMetricsNamespace:
-      process.env.AWS_CLOUDWATCH_METRICS_NAMESPACE ||
-      'lambda-config-set-processor/ApplicationMetrics', //TODO: reimplement throw when infra is ready
-    // ?? throwMissingRequiredEnvVar('AWS_CLOUDWATCH_METRICS_NAMESPACE'),
+      process.env.AWS_CLOUDWATCH_METRICS_NAMESPACE ??
+      throwMissingRequiredEnvVar('AWS_CLOUDWATCH_METRICS_NAMESPACE'),
     lambdaPrefix:
       process.env.LAMBDA_PREFIX ?? throwMissingRequiredEnvVar('LAMBDA_PREFIX'),
   },
