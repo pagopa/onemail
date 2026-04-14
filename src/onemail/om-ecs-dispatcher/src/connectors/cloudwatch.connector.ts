1a492cffb536d18fd534a8fd24be1b8093a02d6e
GiuMontesano <giuseppe.montesano@pagopa.it>
2026-04-14 12:14:22 +0200

    refactor: remove CloudWatch metrics dependency and related configurations

--- src/onemail/om-ecs-dispatcher/src/connectors/cloudwatch.connector.ts
+++
@@ -1,7 +0,0 @@
-import env from '#config/env';
-import { Metrics } from '@aws-lambda-powertools/metrics';
-
-export const metricsClient = new Metrics({
-  namespace: env.aws.servicePrefix,
-  serviceName: env.aws.cloudWatchMetricsNamespace,
-});
