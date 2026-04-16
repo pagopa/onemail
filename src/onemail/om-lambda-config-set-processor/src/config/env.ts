89085f40c04ad85606e5fa753eee7983c7bd0835
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2026-04-16 10:20:11 +0200

    chore: remove redundant region env var for sqs client

--- src/onemail/om-lambda-config-set-processor/src/config/env.ts
+++ src/onemail/om-lambda-config-set-processor/src/config/env.ts
@@ -1,7 +1,6 @@
 export default {
   projectVersion: process.env.npm_package_version || '1.0.0',
   aws: {
-    region: process.env.AWS_REGION ?? throwMissingRequiredEnvVar('AWS_REGION'),
     emailDbTable:
       process.env.AWS_EMAIL_DB_TABLE ??
       throwMissingRequiredEnvVar('AWS_EMAIL_DB_TABLE'),
--- src/onemail/om-lambda-config-set-processor/src/connectors/sqs.connector.ts
+++ src/onemail/om-lambda-config-set-processor/src/connectors/sqs.connector.ts
@@ -1,8 +1,5 @@
-import env from '#config/env';
 import { SQSClient } from '@aws-sdk/client-sqs';
 
-const sqsClient = new SQSClient({
-  region: env.aws.region,
-});
+const sqsClient = new SQSClient({});
 
 export { sqsClient };
