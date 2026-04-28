255d06e39abdaa3efee98c13fd787ed315012b5d
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-04-28 15:41:55 +0200

    fix: update tenant name and configuration set prefixes to use project_nodomain_ses

--- src/infra/src/70_domains/onemail_app/99_locals.tf
+++ src/infra/src/70_domains/onemail_app/99_locals.tf
@@ -1,13 +1,14 @@
 locals {
   project                       = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
   project_nodomain              = "${var.prefix}-${var.env_short}-${var.location_short}"
+  project_nodomain_ses          = "${var.prefix}-${var.env_short}"
   product                       = "${var.prefix}-${var.env_short}"
   zone_name                     = var.env == "prod" ? var.dns_zone_name : "${var.env}.${var.dns_zone_name}"
   tenants_file_path             = "${path.module}/../../data/tenants/tenants.json"
   raw_tenants                   = jsondecode(file(local.tenants_file_path))
   tenant_domain_prefix          = var.env == "prod" ? "" : "${var.env}."
-  tenant_name_prefix            = "${local.project_nodomain}-tenant"
-  configuration_set_name_prefix = "${local.project_nodomain}-configuration-set"
+  tenant_name_prefix            = "${local.project_nodomain_ses}-tenant"
+  configuration_set_name_prefix = "${local.project_nodomain_ses}-configuration-set"
   tenants = {
     for tenant_key, tenant_data in local.raw_tenants : tenant_key => {
       domain                 = "${local.tenant_domain_prefix}${tenant_data.domain}"
